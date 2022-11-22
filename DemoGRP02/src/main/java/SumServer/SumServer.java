7c2ec6bf1735c533c410b91f5584f2727f005091
64070145Rosjana <63070145@kmitl.ac.th>
2022-11-22 11:48:18 +0700

    lab 14

--- DemoGRP02/src/main/java/SumServer/SumServer.java
+++ DemoGRP02/src/main/java/SumServer/SumServer.java
@@ -1,23 +1,25 @@
-package GreetingServer;
+package SumServer;
+
+
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
 
-public class GreetingServer {
+public class SumServer {
     public static void main(String[] args) {
         System.out.println("Hello gRPC");
         Server server = ServerBuilder.forPort(50055)
-                .addService(new GreetServicelmpl())
+                .addService(new SumServicelmpl())
                 .build();
         try {
             server.start();
             System.out.println("Server Start");
-
         }catch (IOException e){
             e.printStackTrace();
         }
+
 
         Runtime.getRuntime().addShutdownHook(new Thread(
                 () -> {
